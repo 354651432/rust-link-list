12ece036634ae8ffa7f38fcdf5e1688cbf944cc8
aniki <aniki@dr.h>
2022-05-21 17:33:32 +0800

    implement link

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,17 @@ use std::{
 };
 
 #[derive(Debug)]
-pub struct Node<T: Copy + Display + std::fmt::Debug> {
+pub struct Node<T> {
     pub data: T,
     pub next: Option<Box<Self>>,
 }
 
-pub struct Link<T: Copy + Display + std::fmt::Debug> {
+pub struct Link<T> {
     pub head: Option<Node<T>>,
     idx: RefCell<usize>,
 }
 
-impl<T: Copy + Display + std::fmt::Debug> Link<T> {
+impl<T: std::cmp::PartialEq> Link<T> {
     pub fn new() -> Self {
         Self {
             head: None,
@@ -61,7 +61,7 @@ impl<T: Copy + Display + std::fmt::Debug> Link<T> {
         }
     }
 
-    pub fn tail(&self) -> Option<T> {
+    pub fn tail(&self) -> Option<&T> {
         let mut tail = self.head.as_ref();
         loop {
             let mut next = &tail.unwrap().next;
@@ -70,10 +70,10 @@ impl<T: Copy + Display + std::fmt::Debug> Link<T> {
             }
             tail = Some(&next.as_ref().unwrap());
         }
-        Some(tail?.data)
+        Some(&tail?.data)
     }
 
-    pub fn my_map(&mut self, f: impl Fn(T) -> T) {
+    pub fn my_map(&mut self, f: impl Fn(&T) -> T) {
         let ret = self.tail();
 
         let mut tail = self.head.as_mut();
@@ -84,7 +84,7 @@ impl<T: Copy + Display + std::fmt::Debug> Link<T> {
 
             let inner = tail.unwrap();
 
-            inner.data = f(inner.data);
+            inner.data = f(&inner.data);
 
             tail = inner.next.as_deref_mut();
         }
@@ -104,12 +104,42 @@ impl<T: Copy + Display + std::fmt::Debug> Link<T> {
         }
     }
 
-    pub fn find(&self, item: T) -> usize {
-        todo!()
+    pub fn find(&self, item: T) -> Option<&Node<T>> {
+        let mut head = self.head.as_ref();
+        loop {
+            if head?.data == item {
+                return head;
+            }
+            head = head?.next.as_ref().map(|it| &**it)
+        }
     }
 
     pub fn insert(&mut self, idx: usize, item: T) {
-        todo!()
+        let mut node = Node::<T> {
+            data: item,
+            next: None,
+        };
+
+        let mut pt = self.head.as_mut();
+        if idx <= 0 {
+            node.next = self.head.take().map(|it| Box::new(it));
+            self.head = Some(node);
+            return;
+        }
+
+        match pt {
+            Some(mut tail) => {
+                for _ in 0..idx - 1 {
+                    tail = match tail.next.as_mut() {
+                        Some(next) => next,
+                        None => return,
+                    };
+                }
+                node.next = tail.next.take();
+                tail.next = Some(Box::new(node));
+            }
+            None => self.head = Some(node),
+        }
     }
 
     pub fn drop(&mut self) {
@@ -121,8 +151,8 @@ impl<T: Copy + Display + std::fmt::Debug> Link<T> {
     }
 }
 
-impl<T: Copy + Display + std::fmt::Debug> Iterator for &Link<T> {
-    type Item = T;
+impl<'a, T> Iterator for &'a Link<T> {
+    type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut p = match &self.head {
@@ -140,6 +170,23 @@ impl<T: Copy + Display + std::fmt::Debug> Iterator for &Link<T> {
             };
         }
         *idx += 1;
-        Some(p.data)
+        Some(&p.data)
+    }
+}
+
+impl<T> Iterator for Link<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let head = self.head.take();
+
+        match head {
+            Some(head) => {
+                let data = head.data;
+                self.head = head.next.map(|it| *it);
+                Some(data)
+            }
+            None => None,
+        }
     }
 }
